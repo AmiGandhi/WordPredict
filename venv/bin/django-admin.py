90acc1bb8a55d0f5194c00be18848f2d87ae7b0a
Krunal Nanda <krunal@colaberry.com>
2019-04-23 19:49:00 -0400

    Model

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/krunal/Northeastern/BDIA/Project/venv/bin/python
+#!/home/krunal/Northeastern/BDIA/Project/venv/bin/python3.6
 from django.core import management
 
 if __name__ == "__main__":
